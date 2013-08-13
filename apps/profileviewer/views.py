7025c058874f7765303e0969fa01c85eca889b97
wenli <spacelis@gmail.com>
2013-08-13 15:01:17 +0200

    Frame work done

--- apps/profileviewer/views.py
+++ apps/profileviewer/views.py
@@ -9,10 +9,46 @@ Description:
     This is the main module for rendering user profiles.
 """
 
+import json
 import twitter
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+
+
+def test_data():
+    """return mock data
+    :returns: @todo
+
+    """
+    data = dict()
+    data['map_data'] = {'center': [48.0, 2.35],
+                        'markers': {
+                            'paris_marker': {'position': 'Paris, France',
+                                'info_window':{
+                                    'content': 'somebla<br>somebla',
+                                    'showOn': 'mouseover',
+                                    'hideOn': 'mouseout',
+                                    }},
+                            'london': {'position': 'London, UK'},
+                        }}
+    data['cate_pie_data'] = [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7]]
+
+    data['cate_timelines_data'] = [
+        ['Year', 'Sales', 'Expenses'],
+        ['2004',  1000,      400],
+        ['2005',  1170,      460],
+        ['2006',  660,       1120],
+        ['2007',  1030,      540]]
+
+    data['poi_pie_data'] = data['cate_pie_data']
+    return {k: json.dumps(v) for k, v in data.iteritems()}
 
 
 def home(request):
@@ -22,5 +58,5 @@ def home(request):
     :returns: @todo
 
     """
-    return render_to_response('viewer.html', {},
+    return render_to_response('viewer.html', test_data(),
                               context_instance=RequestContext(request))
--- static/js/profileviewer/main.js
+++ static/js/profileviewer/main.js
@@ -1,10 +1,42 @@
-function init(){
-  $('#map-canvas').initMap({
-    // Set the center of the map to Paris
-    center: [ 48.861553 , 2.351074 ],
-    markers : {
-      paris_marker: { position: 'Paris, France' },
-      london : { position: 'London, UK' }
-    }
-  });
+function init_map(){
+  $('#map-canvas').initMap(map_data);
+}
+
+function init_cate_pie() {
+  var data = google.visualization.arrayToDataTable(cate_pie_data);
+
+  var options = {
+    //title: 'Category distribution'
+  };
+
+  var chart = new google.visualization.PieChart(
+      document.getElementById('chart-cate-pie'));
+  chart.draw(data, options);
+}
+
+function init_cate_timelines() {
+  var data = google.visualization.arrayToDataTable(cate_timelines_data);
+
+  var options = {
+    title: 'Company Performance',
+    hAxis: {title: 'Time',  titleTextStyle: {color: 'red'}},
+    isStacked: true
+  };
+
+  var chart = new google.visualization.ColumnChart(
+      document.getElementById('chart-cate-timeline'));
+  chart.draw(data, options);
+}
+
+
+function init_poi_pie() {
+  var data = google.visualization.arrayToDataTable(poi_pie_data);
+
+  var options = {
+    //title: 'Category distribution'
+  };
+
+  var chart = new google.visualization.PieChart(
+      document.getElementById('chart-poi-pie'));
+  chart.draw(data, options);
 }
