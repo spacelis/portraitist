2c520309b852e2b63779a1d5dd5dc99793035951
wenli <spacelis@gmail.com>
2013-08-21 00:32:22 +0200

    Fixed all the charts

--- static/js/profileviewer/main.js
+++ static/js/profileviewer/main.js
@@ -1,5 +1,14 @@
+function sum (arr) {
+  var s = 0;
+  for (var i = arr.length - 1; i >= 0; i--) {
+    s += arr[i];
+  }
+  return s;
+}
+
+
 function init_map(map_data){
-  var data = {'center': [41, 100]};
+  var data = {center: [41, -100]};
   var markers = {};
   for(var i in map_data) {
     var poi = map_data[i];
@@ -16,8 +25,13 @@ function init_map(map_data){
   $('#map-canvas').initMap(data);
 }
 
-function init_cate_pie(cate_pie_data) {
-  var data = google.visualization.arrayToDataTable(cate_pie_data);
+
+function init_cate_pie(cate_timeline_data) {
+  var piedata = [['Category', '# of Check-ins']];
+  for (var i in cate_timeline_data) {
+    piedata.push([i, sum(cate_timeline_data[i])]);
+  }
+  var data = google.visualization.arrayToDataTable(piedata);
 
   var options = {
     //title: 'Category distribution'
@@ -28,11 +42,24 @@ function init_cate_pie(cate_pie_data) {
   chart.draw(data, options);
 }
 
+
 function init_cate_timelines(cate_timelines_data) {
-  var data = google.visualization.arrayToDataTable(cate_timelines_data);
+  var tldata = [['# of Check-ins']];
+  var i;
+  for(i in cate_timelines_data) {
+    tldata[0].push(i);
+  }
+  for(i in cate_timelines_data[Object.keys(cate_timelines_data)[0]]){
+    var s = [i,];
+    for(var j in cate_timelines_data) {
+      s.push(cate_timelines_data[j][i]);
+    }
+    tldata.push(s);
+  }
+  var data = google.visualization.arrayToDataTable(tldata);
 
   var options = {
-    title: 'Company Performance',
+    title: 'Category Timelines',
     hAxis: {title: 'Time',  titleTextStyle: {color: 'red'}},
     isStacked: true
   };
@@ -43,8 +70,13 @@ function init_cate_timelines(cate_timelines_data) {
 }
 
 
-function init_poi_pie(poi_pie_data) {
-  var data = google.visualization.arrayToDataTable(poi_pie_data);
+function init_poi_pie(poi_timelines_data) {
+  var piedata = [['POI', '# of Check-ins']];
+  for (var i in poi_timelines_data) {
+    piedata.push(['['+ poi_timelines_data[i].category + ']' + poi_timelines_data[i].name,
+                  sum(poi_timelines_data[i].timeline)]);
+  }
+  var data = google.visualization.arrayToDataTable(piedata);
 
   var options = {
     //title: 'Category distribution'
@@ -52,5 +84,32 @@ function init_poi_pie(poi_pie_data) {
 
   var chart = new google.visualization.PieChart(
       document.getElementById('chart-poi-pie'));
+  chart.draw(data, options);
+}
+
+
+function init_poi_timelines(poi_timelines_data) {
+  var tldata = [['# of Check-ins']];
+  var i;
+  for(i in poi_timelines_data) {
+    tldata[0].push('['+ poi_timelines_data[i].category + ']' + poi_timelines_data[i].name);
+  }
+  for(i in poi_timelines_data[Object.keys(poi_timelines_data)[0]].timeline){
+    var s = [i,];
+    for(var j in poi_timelines_data) {
+      s.push(poi_timelines_data[j].timeline[i]);
+    }
+    tldata.push(s);
+  }
+  var data = google.visualization.arrayToDataTable(tldata);
+
+  var options = {
+    title: 'POI timelines',
+    hAxis: {title: 'Time',  titleTextStyle: {color: 'red'}},
+    isStacked: true
+  };
+
+  var chart = new google.visualization.ColumnChart(
+      document.getElementById('chart-poi-timeline'));
   chart.draw(data, options);
 }
