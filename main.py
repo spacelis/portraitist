9874692ad3f7381594efbc2c59c2a823feefeccc
wenli <spacelis@gmail.com>
2013-09-01 23:29:50 +0200

    Trying to fix running server on Google Appengine

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: main.py
+Author: SpaceLis
+Email: Wen.Li@tudelft.nl
+Github: http://github.com/spacelis
+Description:
+    This file is used to start the server on Google App Engine.
+"""
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
