249b8e3e7b7b00af3d3b2ba65a6cd463fc4d0e8f
wenli <spacelis@gmail.com>
2013-10-16 16:54:07 +0200

    UI: Add judgement submission time and ips on candidate buttons

--- apps/profileviewer/templatetags/extra_tags.py
+++ apps/profileviewer/templatetags/extra_tags.py
@@ -13,8 +13,10 @@ import json
 import urllib2
 
 from django import template
+from django.utils.dateparse import parse_datetime
 
 register = template.Library()
+
 
 @register.filter
 def jsonencode(val):
@@ -36,3 +38,14 @@ def urldecode(val):
 
     """
     return urllib2.unquote(val)
+
+
+@register.filter
+def parsedatetime(val):
+    """ Parse the string (val) for a datetime object
+
+    :val: @todo
+    :returns: @todo
+
+    """
+    return parse_datetime(val)
