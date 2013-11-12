0c02b22904300d197bbf6db99761ce7127ca2c74
wenli <spacelis@gmail.com>
2013-11-12 16:06:03 +0100

    Refactored API Endpoint registration.

--- apps/profileviewer/api.py
+++ apps/profileviewer/api.py
@@ -1,186 +1,123 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
+""" Handling HTTP API calls.
+
 File: api.py
 Author: SpaceLis
 Email: Wen.Li@tudelft.nl
 Github: http://github.com/spacelis
 Description:
     The api endpoints
+
 """
 
 import inspect
-import json
 from json import dumps as _j
+from functools import wraps
+from decorator import decorator
 
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
 
-from apps.profileviewer.models import Expert
-from apps.profileviewer.models import Judge
-from apps.profileviewer.models import TaskPackage
+from google.appengine.ext import ndb
+
+from apps.profileviewer.models import _k
+from apps.profileviewer.models import Judgement
 
 
-def assert_admin(req):
-    """Make sure the call comes from admin
-
-    :req: @todo
-    :returns: @todo
-
-    """
+@decorator
+def secured_request_handler(func, *args, **kwargs):
+    """ Make sure the call comes from admin. """
+    req = kwargs.get('req', None) or args[0]
     if (req.REQUEST.get('_admin_key', None) or
             req.COOKIES.get('_admin_key', None)) != 'tu2013delft':
         raise PermissionDenied
+    return func(*args, **kwargs)
+
+_ENDPOINTS = dict()
 
 
-class EndPoint(object):
+def api_endpoint(endpoint):
+    """ An decorator for API registry.
 
-    """A decorator registry"""
+    Usage:
+        @api_endpoint
+        def an_api(req, a, b):
+            # Do some stuff with a, b
+            return # some stuff
 
-    EndPoints = dict()
+    """
+    argspec = inspect.getargspec(endpoint)
+    _ENDPOINTS[endpoint.__name__] = endpoint, argspec
 
-    def __init__(self, secured=False):
-        """Register an endpoint served in API
+    @wraps
+    def wrapper(*args, **kwargs):
+        """ wrapper. """
+        return endpoint(*args, **kwargs)
 
-        :endpoint: @todo
-
-        """
-        self.secured = secured
-
-    def __call__(self, endpoint):
-        """Calling the wrapped endpoint
-
-        :endpoint: real endpoint handler
-        :returns: @todo
-
-        """
-        EndPoint.EndPoints[endpoint.__name__] = (endpoint, self.secured)
+    return wrapper
 
 
-def call_endpoint(request, endpoint_name):
-    """Call endpoint by name
+def call_endpoint(request, name):
+    """Call endpoint by name.
 
     :request: @todo
     :endpoint_name: @todo
     :returns: @todo
 
     """
-    if endpoint_name not in EndPoint.EndPoints:
+    try:
+        endpoint, argspec = _ENDPOINTS[name]
+        kwargs = {k: request.REQUEST.get(k, None) for k in argspec.args}
+        return HttpResponse(endpoint(**kwargs),  # pylint: disable-msg=W0142
+                            mimetype="application/json")
+    except KeyError:
         raise Http404
-    endpoint, secured = EndPoint.EndPoints[endpoint_name]
-
-    (not secured) or assert_admin(request)  # pylint: disable-msg=W0106
-    argspec = inspect.getargspec(endpoint)
-    args = {k: request.REQUEST.get(k, None) for k in argspec.args}
-    return HttpResponse(endpoint(**args),  # pylint: disable-msg=W0142
-                        mimetype="application/json")
 
 
-@EndPoint(secured=False)
-def expert_checkins(hash_id=None):
-    """Return all checkins for the expert
+@api_endpoint
+@secured_request_handler
+def checkins(candidate):
+    """ Return all checkins for the candidate.
 
-    :hash_id: The hashlib.sha1 of twitter screen_name
+    :candidate: The urlsafe key to the TwitterAccount.
     :returns: All checkins from the database made by the twitter user
 
     """
-    if hash_id:
-        checkins = Expert.getCheckinsInJson(
-            Expert.getExpertByHashId(hash_id)
-        )
-        return checkins
-    return _j({'error': 'Please specify either a '
-              'screen_name or comma separated names.'})
+    try:
+        c = _k(candidate)
+        assert c.kind == 'TwitterAccount'
+        return _j(c.get().checkins)
+    except AssertionError:
+        return _j({'error': 'Please specify either a '
+                   'screen_name or comma separated names.'})
 
 
-@EndPoint(secured=True)
-def sync_judgement():
-    """Update Expert judged_by and judgement_no from Judge entities
-    :returns: @todo
-
-    """
-    for e in Expert.query().fetch():
-        e.judged_by = list()
-        e.judged_no = 0
-        e.put()
-    for j in Judge.query().fetch():
-        if isinstance(j.judgements, unicode):
-            j.judgements = json.loads(j.judgements)
-            j.put()
-        j.judgement_no = len(j.judgements)
-        j.put()
-        assert isinstance(j.judgements, list), \
-            'j.judgements is of type %s' % (type(j.judgements), )
-        for ju in j.judgements:
-            e = Expert.query(Expert.screen_name == ju['candidate']).fetch(1)[0]
-            e.judged_by.append(j.judge_id)  # pylint: disable-msg=E1101
-            e.judged_no += 1
-            e.put()
-    return _j({'msg': 'Sucesses!'})
-
-
-@EndPoint(secured=True)
-def ensure_datatype():
-    """ When upload datastore finished, this function should be called to make
-        sure the data types are correct.
-    :returns: @todo
-
-    """
-    for j in Judge.query().fetch():
-        if isinstance(j.judgements, str) or isinstance(j.judgements, unicode):
-            j.judgements = json.loads(j.judgements)
-            j.put()
-    return _j({'msg': 'Sucesses!'})
-
-
-@EndPoint(secured=True)
-def view_judge(judge_id):
-    """ Return all judgements made by the judge
-
-    :judge_id: The judge_id of a Judge
-    :returns: All Judgement from the Judge in JSON
-
-    """
-    j = Judge.query(Judge.judge_id == judge_id)\
-        .fetch(1)[0]
-    return _j({
-        'judge_id': j.judge_id,
-        'email': j.email,
-        'nick': j.nickname,
-        'judgements': j.judgements,
-        'judgement_no': j.judgement_no
-    })
-
-
-@EndPoint(secured=True)
+@api_endpoint
+@secured_request_handler
 def export_judgements():
-    """
-    :returns: @todo
+    """ Export all judgements as json object per line.
+
+    :returns: An iterator going over lines of json objects
 
     """
+    # pylint: disable-msg=W0212
     def iter_judgement():
         """ An iterator over all judgements """
-        for j in Judge.query().fetch():
-            for ju in j.judgements:
-                ju['judge_id'] = j.judge_id
-                ju['judge_email'] = j.email
-                ju['judge_nick'] = j.nickname
-                yield _j(ju) + '\n'
+        for j in Judgement.query().fetch():
+            yield _j({
+                k: v.urlsafe()
+                if isinstance(Judgement._properties[k], ndb.Key)
+                else v
+                for k, v in j.to_dict()
+            }) + '\n'
     return iter_judgement()
+    # pylint: enable-msg=W0212
 
 
-@EndPoint(secured=True)
-def create_tps():
-    """ Create a list of task packages from undone tasks
-    :returns: @todo
-
-    """
-    return _j(TaskPackage.construct())
-
-
-@EndPoint(secured=True)
+@api_endpoint
+@secured_request_handler
 def assert_error():
-    """ Bring a debug page for console
-    """
+    """ Bring a debug page for console. """
     assert False
