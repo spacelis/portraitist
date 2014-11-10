8b0319e966b22df6f34c37513d8ecb01490e15ce
wenli <spacelis@gmail.com>
2014-11-10 13:49:41 +0100

    Add viewport dims in control.js

--- static/profileviewer/js/control.js
+++ static/profileviewer/js/control.js
@@ -20,12 +20,18 @@ function _control($){
   var pageheight = $(document).height();
   var pagewidth = $(document).width();
   var percentwidth = pagewidth / 100;
+  var viewportwidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var viewportheight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-  var state = {m_clicks: 0,
-               m_travel: 0,
-               prev_pos: {PageX: 0, PageY: 0},
-               init: true,
-               trace: []};
+  var state = {
+    m_clicks: 0,
+    m_travel: 0,
+    viewportwidth: viewportwidth,
+    viewportheight: viewportheight,
+    prev_pos: {PageX: 0, PageY: 0},
+    init: true,
+    trace: []
+  };
   var bar = {
     m_clicks: 3,
     m_travel: pageheight,
@@ -38,30 +44,36 @@ function _control($){
 
   $(document).click(function(e){
     state.m_clicks += 1;
-    state.trace.unshift({event: "click",
-                         position: {"PageX": e.PageX, "PageY": e.PageY},
-                         timestamp: new Date() - loadtime,
-                         travel: state.trace[0].travel});
+    state.trace.unshift({
+      event: "click",
+      position: {"PageX": e.PageX, "PageY": e.PageY},
+      timestamp: new Date() - loadtime,
+      travel: state.trace[0].travel
+    });
   });
   $(document).mousemove(function(e){
     var timestamp = new Date() - loadtime;
     var pos = {PageX: e.pageX, PageY: e.pageY};
     if(state.init){
       state.init = false;
-      state.trace.unshift({event: "moveto",
-                           position: pos,
-                           timestamp: timestamp,
-                           travel: 0});
+      state.trace.unshift({
+        event: "moveto",
+        position: pos,
+        timestamp: timestamp,
+        travel: 0
+      });
     } else{
       state.m_travel += distance(pos, state.prev_pos);
       state.prev_pos = pos;
       if(distance(state.trace[0].position, pos) > percentwidth ||
          timestamp - state.trace[0].timestamp > 1000)
       {
-        state.trace.unshift({event: "moveto",
-                             position: pos,
-                             timestamp: timestamp,
-                             travel: state.m_travel});
+        state.trace.unshift({
+          event: "moveto",
+          position: pos,
+          timestamp: timestamp,
+          travel: state.m_travel
+        });
       }
     }
   });
