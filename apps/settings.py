18ba747ef7d777dd58fbdd84f5ecd15242414253
wenli <spacelis@gmail.com>
2014-11-18 12:18:51 +0100

    Add two more allowed_host

--- apps/settings.py
+++ apps/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['geo-expertise.appspot.com', 'localhost']
+ALLOWED_HOSTS = ['geo-expertise.appspot.com', 'localhost', '82.95.189.142', 'e6510']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
